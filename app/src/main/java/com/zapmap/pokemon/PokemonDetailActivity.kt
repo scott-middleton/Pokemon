b6c2b5cf57358750f5091d616380384f17d512eb
Ivan Wooll <ivanwooll@zap-map.com>
2022-11-21 14:35:47 +0000

    Cheeky rewrite of the git history?

---
+++ app/src/main/java/com/zapmap/pokemon/PokemonDetailActivity.kt
@@ -0,0 +1,55 @@
+package com.zapmap.pokemon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import coil.load
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.zapmap.pokemon.databinding.ActivityPokemonDetailBinding
+
+class PokemonDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPokemonDetailBinding
+
+    private lateinit var remotePokemonItem: RemotePokemonItem
+
+    private val moshi = Moshi.Builder().build()
+    private val jsonAdapter: JsonAdapter<RemotePokemonItem> = moshi.adapter(RemotePokemonItem::class.java)
+
+    private val typesAdapter = TypesAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPokemonDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        intent?.getStringExtra("json")?.let {
+            remotePokemonItem = jsonAdapter.fromJson(it)!!
+        }
+
+        lifecycleScope.launchWhenCreated {
+            val stringId = remotePokemonItem.url.removeSuffix("/").substringAfterLast("/")
+            val id = stringId.toInt()
+            val response = Api.getApi().fetchPokemonById(id)
+            if (response.isSuccessful) {
+                displayPokemon(response.body()!!)
+            }
+        }
+        binding.recyclerViewTypes.adapter = typesAdapter
+
+        binding.textViewPokemonName.text = remotePokemonItem.name
+    }
+
+    private fun displayPokemon(pokemon: RemotePokemon) {
+        binding.apply {
+            textViewPokemonName.text = pokemon.name.capitalize()
+            textViewWeight.text = pokemon.weight.toString()
+            textViewHeight.text = pokemon.height.toString()
+            imageViewPokemonFront.load(pokemon.sprites.frontDefault)
+            typesAdapter.updateItems(pokemon.types.map { it.type.name })
+        }
+    }
+}
